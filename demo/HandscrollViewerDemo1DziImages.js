18392e3e82522c7fe9287e51c77cdaf1963e2f50
maggielhlyc <maggielhlyc@gmail.com>
2026-03-28 19:25:09 +0800

    zhangtong2

--- demo/HandscrollViewerDemo1DziImages.js
+++ demo/HandscrollViewerDemo1DziImages.js
@@ -1,11 +1,9 @@
 
-// siteDziImages array for Zhang Tong Handscroll
-var siteDziImages = [
+/* Zhang tong - 4K 展陈数据配置文件 */
+var handscrollViewerDziImages = [
     {
-        // 张 同（清） 仿周之冕花卉卷-全卷 (Zhang Tong Handscroll)
-        Id: "Zhang_Tong_Handscroll",      // 你的长卷 ID
-        Url: "E:\Github\shoujuan-zoomify/",                   // 极其重要：指向根目录，因为你的切片和 XML 都在那里
-        Height: "1146",               // 你的长卷高度
-        Width: "21119",               // 你的长卷宽度
+        "id": "zhangtong",
+        "title": "zhangtong手卷",
+        "tileSources": "../ImageProperties.xml"
     }
 ];
