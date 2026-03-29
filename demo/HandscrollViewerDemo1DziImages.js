cc95d646f7142e43f8c6dcf209eece31b3122430
maggielhlyc <maggielhlyc@gmail.com>
2026-03-29 23:04:44 +0800

    NEW MU

--- demo/HandscrollViewerDemo1DziImages.js
+++
@@ -1,9 +0,0 @@
-
-/* Zhang tong - 4K 展陈数据配置文件 */
-var handscrollViewerDziImages = [
-    {
-        "id": "zhangtong",
-        "title": "zhangtong手卷",
-        "tileSources": "../ImageProperties.xml"
-    }
-];
